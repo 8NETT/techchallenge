e6c5694d69b0ecb86adeb677dd13e8ed7e8bcc0e
Rayan Lima <rayan.lima@evup.com.br>
2025-05-29 19:55:28 -0300

    wrapping methods in a try-catch block

--- TechChallengeApi/Controllers/AccountController.cs
+++ TechChallengeApi/Controllers/AccountController.cs
@@ -25,23 +25,46 @@ namespace TechChallenge.Controllers
         }
 
         [Authorize]
-        public IActionResult Biblioteca() =>
-            Ok(_usuarioRepository.ObterJogosPorUsuario(User.GetId()));
+        [HttpGet]
+        public IActionResult Biblioteca()
+        {
+            try
+            {
+                return Ok(_usuarioRepository.ObterJogosPorUsuario(User.GetId()));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new
+                {
+                    error = e.Message
+                });
+            }
+        }
 
         [HttpPost("login")]
         public async Task<IActionResult> Login(LoginModel model)
         {
-            if (!ModelState.IsValid)
-                return BadRequest();
+            try
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest();
 
-            var usuario = await _usuarioRepository.ObterPorEmailAsync(model.Email);
+                var usuario = await _usuarioRepository.ObterPorEmailAsync(model.Email);
 
-            if (usuario == null)
-                return Unauthorized();
-            if (!_passwordHasher.Verify(model.Password, usuario.Password))
-                return Unauthorized();
+                if (usuario == null)
+                    return Unauthorized();
+                if (!_passwordHasher.Verify(model.Password, usuario.Password))
+                    return Unauthorized();
 
-            return Ok(_jwtService.GenerateToken(usuario));
+                return Ok(_jwtService.GenerateToken(usuario));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new
+                {
+                    error = e.Message
+                });
+            }
         }
     }
 }
